06dc3bac971254ecc29d919d225f30ce94c00050
Abinithishkumarprabakaran <abinithishkumarp.a.k@gmail.com>
2023-05-14 14:03:59 +0530

    launch

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BsTwitter, BsInstagram, BsGithub } from 'react-icons/bs';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
+
+const SocialMedia = () => {
+  return (
+    <div className='app__social'>
+      <div>
+        <FiMail />
+      </div>
+      <div>
+        <FaLinkedinIn />
+      </div>
+      <div>
+        <BsInstagram />
+      </div>
+      <div>
+        <BsGithub />
+      </div>
+      <div>
+        <BsTwitter />
+      </div>
+    </div>
+  )
+}
+
+export default SocialMedia
